8a477b5404a56c997bcc74e5c7705d676e9912e4
Joeway Banh <joewaybanh@hotmail.com>
2021-05-20 14:43:05 +0200

    added animation with manual coordinations

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const mouse = document.querySelector(".cursor");
 const mouseText = document.querySelector("span");
+const paperPlane = document.querySelector(".paper-plane");
+const section2 = document.querySelector(".section-2");
+const endText = document.querySelector("end-text");
 
 const cursor = (e) => {
   mouse.style.top = e.pageY + "px";
@@ -8,20 +11,39 @@ const cursor = (e) => {
 
 const cursorActive = (e) => {
   const item = e.target;
-  //   if (item.id === "logo" || item.classList.contains("burger")) {
-  //     mouse.classList.add("nav-active");
-  //   } else {
-  //     mouse.classList.remove("nav-active");
-  //   }
+
   if (item.classList.contains("to-top")) {
-    // mouseText.classList.add("to-top-active");
     gsap.to(".text-swipe", 1, { y: "0%" });
-    // mouseText.innerText = "Tap";
   } else {
     mouseText.classList.remove("to-top-active");
-    // mouseText.innerText = "";
     gsap.to(".text-swipe", 1, { y: "100%" });
   }
 };
 window.addEventListener("mousemove", cursor);
 window.addEventListener("mouseover", cursorActive);
+//initialising the controller with ScrollMagic
+const controller = new ScrollMagic.Controller();
+//using gsap
+const planeTimeline = gsap.timeline({
+  defaults: {
+    duration: 1,
+    ease: "power2.inOut",
+    autoRotate: true,
+  },
+});
+planeTimeline.fromTo(
+  paperPlane,
+  { x: "-10%", y: "0%", rotation: "0" },
+  { x: "300%", y: "200%", rotation: "10" }
+);
+planeTimeline.to(paperPlane, { x: "400%", y: "200%", rotation: "-10" });
+planeTimeline.to(paperPlane, { x: "900%", y: "-400%", rotation: "-80" });
+//creating a scene
+const planeScene = new ScrollMagic.Scene({
+  duration: 3000,
+  triggerElement: section2,
+  triggerHook: 0,
+})
+  .setTween(planeTimeline)
+  .addTo(controller)
+  .setPin(section2);
