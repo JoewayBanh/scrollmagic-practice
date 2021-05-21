0602a9d3d38cd6aa4159e05b788d6b634677202b
Joeway Banh <joewaybanh@hotmail.com>
2021-05-21 20:40:21 +0200

    added animations to section 1 (the one with the 'hover me')

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const paperPlane = document.querySelector(".paper-plane");
 const section2 = document.querySelector(".section-2");
 const endText = document.querySelector(".end-text");
 const endTextSection = document.querySelector("footer");
+const header = document.querySelector("header");
+const headerText = header.querySelector("h1");
+const section1title = document.querySelector(".section-1-title");
+const spans = document.querySelectorAll("span");
 
 const cursor = (e) => {
   mouse.style.top = e.pageY + "px";
@@ -15,9 +19,13 @@ const cursorActive = (e) => {
 
   if (item.classList.contains("to-top")) {
     gsap.to(".text-swipe", 1, { y: "0%" });
+    gsap.to(section1title, 1, { scale: 2 });
+    gsap.to(spans, 1, { x: "0%" });
   } else {
     mouseText.classList.remove("to-top-active");
     gsap.to(".text-swipe", 1, { y: "100%" });
+    gsap.to(section1title, 1, { scale: 1 });
+    gsap.to(spans, 1, { x: "10%" });
   }
 };
 window.addEventListener("mousemove", cursor);
@@ -40,7 +48,8 @@ planeTimeline.fromTo(
   "-=.5"
 );
 planeTimeline.to(paperPlane, { x: "400%", y: "200%", rotation: "-10" });
-planeTimeline.to(paperPlane, { x: "900%", y: "-400%", rotation: "-80" });
+planeTimeline.to(paperPlane, { x: "1200%", y: "-400%", rotation: "-50" });
+
 //creating a scene
 const planeScene = new ScrollMagic.Scene({
   duration: 3000,
@@ -64,4 +73,21 @@ endScene = new ScrollMagic.Scene({
   triggerHook: 0.5,
 })
   .setTween(endTimeline)
+  .addTo(controller);
+
+const headerTimeline = gsap.timeline({
+  defaults: { duration: 1, ease: "power1.inOut" },
+});
+headerTimeline.fromTo(
+  headerText,
+  { opacity: "1", scale: "1" },
+  { opacity: "0", scale: "0" }
+);
+headerScene = new ScrollMagic.Scene({
+  triggerElement: header,
+  triggerHook: 0.1,
+  duration: "100%",
+})
+  .setTween(headerTimeline)
+  // .addIndicators()
   .addTo(controller);
